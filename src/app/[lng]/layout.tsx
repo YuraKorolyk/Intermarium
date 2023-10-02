ee0c9f156a7e3678aa96e566c360a23bab711c62
YuraKorolyk <yura.korolyk@gmail.com>
2023-10-03 00:19:36 +0300

    add Pixel

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -38,8 +38,26 @@ export default async function RootLayout({
       })(window,document,'script','dataLayer','GTM-KPM5FN4R');
       `}
     </Script>
-
+    <Script id="Pixel">
+      {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1258095921546445');
+      fbq('track', 'PageView');
+      `}
+    </Script>
     <body>
+    <noscript>
+      <img height="1" width="1" style={{display: "none"}}
+           src="https://www.facebook.com/tr?id=1258095921546445&ev=PageView&noscript=1"
+      />
+    </noscript>
     <noscript>
       <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPM5FN4R"
               height="0" width="0"
